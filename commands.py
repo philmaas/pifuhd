7a7f61461d618cfc910be5e8cf9bcb79bb5a1b10
philmaas <contact@philippmaas.com>
2023-03-07 11:52:58 +0100

    add visualization command

--- commands.py
+++ commands.py
@@ -4,3 +4,7 @@ python ../lightweight-human-pose-estimation.pytorch/crop_imagePose.py
 python -m apps.simple_test -r 512 --use_rect -i ./sample_images/
 
 python apps/clean_mesh.py -f ./results/pifuhd_final/recon/
+
+
+python -m apps.render_turntable -f ./results/pifuhd_final/recon/ -ww 512 -hh 512
+# add -g for geometry rendering. default is normal visualization.
